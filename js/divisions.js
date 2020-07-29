94fb58123ef5c00d84cdd049abdbb23db46b65ae
somadatta9874 <66265341+somadatta9874@users.noreply.github.com>
2020-07-29 22:00:32 +0530

    Add files via upload

---
+++ js/divisions.js
@@ -0,0 +1,20 @@
+class Divisions {
+    constructor(x, y, w, h) {
+        var options = {
+
+            isStatic: true
+        }
+        this.body = Bodies.rectangle(x, y, w, h, options);
+        this.w = w;
+        this.h = h;
+        World.add(world, this.body);
+        textSize(35)
+    text("500",100,300)
+    }
+    display() {
+        var pos = this.body.position;
+        rectMode(CENTER);
+        fill("white");
+        rect(pos.x, pos.y, this.w, this.h);
+    }
+};
